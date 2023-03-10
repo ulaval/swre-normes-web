fc721efb05a40541de6796db4d80a74699f54837
Alexandre Breton <alexandre.breton.3@ulaval.ca>
2023-03-10 09:38:59 -0500

    more spacing for lisibility

--- src/js/normes_ul.js
+++ src/js/normes_ul.js
@@ -1,7 +1,7 @@
 // Fermeture du menu
 var closeMainMenu = function() {
   document.querySelector(".ulaval-header-mobile-menu").classList.remove("open");
-  document.querySelector(".ulaval-header-menu-trigger").querySelector("span.screen-reader-text").innerText="Ouvrir le menu principal";
+  document.querySelector(".ulaval-header-menu-trigger").querySelector("span.screen-reader-text").innerText = "Ouvrir le menu principal";
   document.querySelector(".ulaval-header-menu-trigger").setAttribute("aria-expanded","false");
   document.body.classList.remove("ulaval-nav-mobile-open");
 }
@@ -15,31 +15,31 @@ var closeMainSearch = function() {
 
 // Fermeture du sélecteur de langue
 var closeLanguageSwitcher = function() {
-  document.getElementById("ulaval-language-switcher-list").hidden=true;
+  document.getElementById("ulaval-language-switcher-list").hidden = true;
   document.querySelector(".ulaval-language-switcher-trigger").setAttribute("aria-expanded","false");
-  if(currentlyFocusedLang=document.querySelector("#ulaval-language-switcher-list>li[aria-selected=true]")) {
+  if(currentlyFocusedLang = document.querySelector("#ulaval-language-switcher-list>li[aria-selected=true]")) {
     currentlyFocusedLang.setAttribute("aria-selected","false");
   }
   document.removeEventListener('keyup', languageSwitcherKeys);
   document.removeEventListener('keydown', preventScrollOnNavKeyDowns);
   document.removeEventListener('click', detectClickOutside);
   const switcherItems = document.querySelectorAll("#ulaval-language-switcher-list>li a");
-  switcherItems.forEach(item=>{
+  switcherItems.forEach(item => {
     item.removeEventListener("mouseover",blurSwitcherItems);
   });
 }
 
 // Comportement du bouton de menu mobile
-document.querySelector(".ulaval-header-menu-trigger").addEventListener("click",function() {
-  if(this.getAttribute("aria-expanded")=="false") {
-    if(searchBarButton=document.querySelector(".ulaval-header-search-trigger")) {
-      if(searchBarButton.getAttribute("aria-expanded")=="true") {
+document.querySelector(".ulaval-header-menu-trigger").addEventListener("click", function() {
+  if(this.getAttribute("aria-expanded") == "false") {
+    if(searchBarButton = document.querySelector(".ulaval-header-search-trigger")) {
+      if(searchBarButton.getAttribute("aria-expanded") == "true") {
         closeMainSearch();
       }
     }
 
     document.querySelector(".ulaval-header-mobile-menu").classList.add("open");
-    this.querySelector("span.screen-reader-text").innerText="Fermer le menu principal";
+    this.querySelector("span.screen-reader-text").innerText = "Fermer le menu principal";
     this.setAttribute("aria-expanded","true");
     document.body.classList.add("ulaval-nav-mobile-open");
   } else {
@@ -50,10 +50,10 @@ document.querySelector(".ulaval-header-menu-trigger").addEventListener("click",f
 
 
 // Comportement du bouton du moteur de recherche principal
-if(searchBarButton=document.querySelector(".ulaval-header-search-trigger")) {
-  searchBarButton.addEventListener("click",function() {
-    if(this.getAttribute("aria-expanded")=="false") {
-      if(document.querySelector(".ulaval-header-menu-trigger").getAttribute("aria-expanded")=="true") {
+if(searchBarButton = document.querySelector(".ulaval-header-search-trigger")) {
+  searchBarButton.addEventListener("click", function() {
+    if(this.getAttribute("aria-expanded") == "false") {
+      if(document.querySelector(".ulaval-header-menu-trigger").getAttribute("aria-expanded") == "true") {
         closeMainMenu();
       }
     document.querySelector(".ulaval-header-search").classList.add("open");
@@ -67,8 +67,8 @@ if(searchBarButton=document.querySelector(".ulaval-header-search-trigger")) {
 
 // Comportement du sélecteur de langue
 if(languageSwitcher = document.querySelector(".ulaval-language-switcher-trigger")) {
-  languageSwitcher.addEventListener("click",function() {
-    if(this.getAttribute("aria-expanded")=="false") {
+  languageSwitcher.addEventListener("click", function() {
+    if(this.getAttribute("aria-expanded") == "false") {
       document.getElementById("ulaval-language-switcher-list").hidden = false;
       this.setAttribute("aria-expanded","true");
       document.querySelector("#ulaval-language-switcher-list>li:first-child a").focus();
@@ -77,7 +77,7 @@ if(languageSwitcher = document.querySelector(".ulaval-language-switcher-trigger"
       document.addEventListener('keydown', preventScrollOnNavKeyDowns);
       document.addEventListener('click', detectClickOutside);
       const switcherItems = document.querySelectorAll("#ulaval-language-switcher-list>li a");
-      switcherItems.forEach(item=>{
+      switcherItems.forEach(item => {
         item.addEventListener("mouseover",blurSwitcherItems);
       });
       } else {
@@ -88,8 +88,8 @@ if(languageSwitcher = document.querySelector(".ulaval-language-switcher-trigger"
 
 var blurSwitcherItems = function(e) {
   const switcherItems = document.querySelectorAll("#ulaval-language-switcher-list>li a");
-  switcherItems.forEach(item=>{
-    if(item==document.activeElement) {
+  switcherItems.forEach(item => {
+    if(item= = document.activeElement) {
       item.blur();
     }
   });
@@ -110,7 +110,7 @@ var preventScrollOnNavKeyDowns = function(e) {
 
 // Détection des touches pour la navigation du sélecteur de langue par clavier
 var languageSwitcherKeys = function(e) {
-  const currentFocus=document.activeElement.parentElement;
+  const currentFocus = document.activeElement.parentElement;
   const currentIndex=[...currentFocus.parentElement.children].indexOf(currentFocus);
   switch(e.keyCode) {
     case 38: // Up

71bb07597070f71009241a72ca66e0ead0500dd8
Alexandre Breton <alexandre.breton.3@ulaval.ca>
2023-04-03 11:30:02 -0400

    fix wrong rgba

--- webpack.config.js
+++ webpack.config.js
@@ -1,149 +1,45 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const StylelintPlugin = require('stylelint-webpack-plugin');
-const WebpackShellPluginNext = require('webpack-shell-plugin-next');
-const fs = require('fs');
 const path = require('path');
-const { globSync } = require('glob')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// Common Configuration
-var config = {
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							//sourceMap: true,
-							url: false
-						}
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sassOptions: {
-								//outputStyle: 'compressed',
-								sourceMap: true
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							emitFile: false, // Éviter que les fichiers soient copiés dans le dossier "build". On gére le tout manuellement.
-							name: '[path][name].[ext]', // Conserver le nom des fichiers.
-						},
-					},
-				],
-			},
-			// {
-			// 	test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
-			// 	use: [
-			// 	  {
-			// 		loader: 'file-loader',
-			// 		options: {
-			// 		  name: '[name].[ext]',
-			// 		  outputPath: 'fonts/'
-			// 		}
-			// 	  }
-			// 	]
-			// },
-			{
-				enforce: 'pre',
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					// options: {
-					// 	//plugins: ['lodash'],
-					// 	presets: [
-					// 		["@babel/preset-env", {
-					// 			"targets": {
-					// 				"browsers": [
-					// 					"last 2 versions"
-					// 				]
-					// 			},
-					// 			"useBuiltIns": "usage",
-					// 			"corejs": "3"
-					// 		}]
-					// 	],
-					// 	"plugins": [
-					// 	],
-					// 	cacheDirectory: true
-					// }
-				}
-			}
-			// Linting JavaScript à l'enregistrement des fichiers
-			/*{
-				enforce: 'pre',
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'eslint-loader',
-			},*/
-		],
-	},
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.scss', '.sass']
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.s?css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  }
 };
-
-// CSS Configuration
-var cssConfig = Object.assign({}, config, {
-    name: "css",
-    entry:
-		globSync('./src/scss/**/[^_]*.scss').reduce(function(obj, el){
-			obj[path.parse(el).name] = './' + el;
-			return obj
-		},{}),
-    output: {
-      path: path.resolve(__dirname, 'assets/tmp'),
-      },
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '../../assets/css/[name].css',
-		}),
-		// Linting CSS
-		new StylelintPlugin({
-			lintDirtyModulesOnly: true,
-			failOnError: false,
-			failOnWarning: false,
-		}),
-		new WebpackShellPluginNext({
-            onBuildEnd: {
-                scripts: [
-                    () => {
-                        fs.rmSync("./assets/tmp", { recursive: true });
-                    }
-                ]
-            }
-        })
-	],
-});
-
-// JS Configuration
-var jsConfig = Object.assign({}, config,{
-	name: "js",
-	entry:
-		globSync('./src/js/**.js').reduce(function(obj, el){
-			obj[path.parse(el).name] = './' + el;
-			return obj
-		},{}),
-	output: {
-		path: path.resolve(__dirname, './assets/js'),
-		filename: "[name].js"
-	},
-});
-
-// Return Array of Configurations
-module.exports = [
-	cssConfig, jsConfig,
-];
-
-
-
